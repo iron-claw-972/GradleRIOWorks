d42426e87de858858f20b632a50976a67e4db15e
me1234q <me1234q@gmail.com>
2019-11-04 15:46:10 -0800

    State Machines Kind of Make Sense

--- src/main/java/frc/team972/robot/statemachines/ExampleState.java
+++ src/main/java/frc/team972/robot/statemachines/ExampleState.java
@@ -3,15 +3,14 @@ package frc.team972.robot.statemachines;
 public enum ExampleState {
     ON, OFF;
 
-    public ExampleState next(ExampleState state) {
-        switch (state) {
+    public ExampleState next() {
+        switch (this) {
             case ON:
                 return OFF;
                 break;
             case OFF:
                 return ON;
-                break;
-                
+                break;        
         }
     }
 }
